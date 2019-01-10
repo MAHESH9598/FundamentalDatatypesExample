dbec9c114a132e4a9cf56bed520aed7e5c606b87
Mr. Mahesh Sunkara <46166792+MAHESH9598@users.noreply.github.com>
2019-01-10 12:29:47 +0530

    Initial commit

---
+++ FundamentalDatatypesExample.py
@@ -0,0 +1,25 @@
+a=2000
+print(a)
+print(type(a))
+print(id(a))
+
+b=123.123
+print(b)
+print(type(b))
+print(id(b))
+
+c=2+6j
+print(c)
+print(type(c))
+print(id(c))
+
+d=True
+print(d)
+print(type(d))
+print(id(d))
+
+e='Love You Ra Mahesh'
+print(e)
+print(type(e))
+print(id(e))
+
